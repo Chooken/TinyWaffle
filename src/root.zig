d59b4046f5b6bc0b32f6fde7ce91c16ac41146cb
Chooken <joshuawereszczuk@gmail.com>
2026-03-10 01:06:41 +1000

    Update root.zig

--- src/root.zig
+++ src/root.zig
@@ -105,8 +105,8 @@ pub fn Rect(comptime T: type) type {
             const self_min = self.min();
             const self_max = self.max();
 
-            if (other.x < self_min.x or other.y <= self_min.y or 
-                other.x > self_max.x or other.y >= self_max.y)
+            if (other.x < self_min.x or other.y < self_min.y or 
+                other.x >= self_max.x or other.y >= self_max.y)
             {
                 return false;
             }
