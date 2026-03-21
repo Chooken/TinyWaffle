0f2be6e187c3be702b00de1fd62aa926c63fb7d0
Chooken <54669830+Chooken@users.noreply.github.com>
2026-03-21 16:32:48 +1000

    Update window.zig

--- src/window.zig
+++ src/window.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const sdl3 = @import("sdl3");
+const root = @import("root.zig");
 const internal = @import("internal.zig");
 const assert = @import("assert.zig");
 
@@ -18,6 +19,11 @@ pub fn GetSettings() WindowSettings {
 
 pub fn SetFullscreen(value: bool) void {
     assert.ok(internal.application.sdl_window.setFullscreen(value));
+}
+
+pub fn GetSize() root.Vec2(usize) {
+    const data: struct { usize, usize } = assert.ok(internal.application.sdl_window.getSizeInPixels());
+    return .{ .x = data.@"0", .y = data.@"1" };
 }
 
 pub fn GetDensity() f32 {
